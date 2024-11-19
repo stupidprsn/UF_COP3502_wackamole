c4f3307885069c959e0d3255fa83406c493ffc3b
stupidprsn <hanlin1581@gmail.com>
2024-11-19 17:12:00 -0500

    Revert "Finish"
    This reverts commit 4c014cf520b068d51aaa6b93741aec1e987c22a2.

--- assignment.py
+++
@@ -1,8 +0,0 @@
-# Hanlin Zhang
-# Nov. 19, 2024
-
-class Assignment:
-    def __init__(self, name: str = '', code: str = '', points: str = '') -> None:
-        self.name: str = name
-        self.code: str = code
-        self.points: str = points
